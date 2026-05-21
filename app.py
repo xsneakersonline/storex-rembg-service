b52f5360da13c3713eb45a530c63e05365e7132f
xsneakersonline <xsneakersonline@gmail.com>
2026-05-22 01:56:58 +0400

    Load rembg model lazily per request

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import Response
-from rembg import new_session
 
 app = FastAPI()
-session = new_session("u2netp")
+session = None
 
 
 @app.get("/")
@@ -13,7 +12,11 @@ def health():
 
 @app.post("/api/remove")
 async def remove_background(file: UploadFile = File(...)):
-    from rembg import remove
+    global session
+    from rembg import new_session, remove
+
+    if session is None:
+        session = new_session("u2netp")
 
     source = await file.read()
     output = remove(source, session=session)
