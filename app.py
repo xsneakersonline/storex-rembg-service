856afb4732c52308116e5793c8ff8b1b7625a1be
xsneakersonline <xsneakersonline@gmail.com>
2026-05-22 01:46:30 +0400

    Use lightweight rembg model

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import Response
+from rembg import new_session
 
 app = FastAPI()
+session = new_session("u2netp")
 
 
 @app.get("/")
@@ -11,9 +13,8 @@ def health():
 
 @app.post("/api/remove")
 async def remove_background(file: UploadFile = File(...)):
-    # Import lazily so Render can bind the HTTP port before rembg/model startup work.
     from rembg import remove
 
     source = await file.read()
-    output = remove(source)
+    output = remove(source, session=session)
     return Response(content=output, media_type="image/png")
